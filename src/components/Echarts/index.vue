530233fe9b43ebd81dd78147de09b992c61363bd
wujiatong <17662073174@163.com>
2025-07-02 17:47:54 +0800

    refactor(components): 重构 Echarts 组件代码
    - 格式化代码，使其符合规范的 TypeScript 语法
    - 优化导入语句，使用绝对路径替代相对路径
    - 简化部分代码逻辑，提高可读性
    - 更新 package.json 文件版本号至0.1.0

--- src/components/Echarts/index.vue
+++ src/components/Echarts/index.vue
@@ -1,132 +1,137 @@
 <script setup lang="ts">
-import type { ECElementEvent, EChartsType } from 'echarts/core'
-import type { ECOption } from './config'
-import {useDebounceFn, useResizeObserver} from '@vueuse/core'
-import echarts from './config'
+import type { ECElementEvent, EChartsType } from "echarts/core";
+import type { ECOption } from "./config";
+import { useDebounceFn, useResizeObserver } from "@vueuse/core";
+import echarts from "./config";
 import {
-  ref,
-  shallowRef,
-  computed,
-  watch,
-  markRaw,
-  onMounted,
-  nextTick,
-  onActivated,
-  onBeforeUnmount,
-  watchSyncEffect
+	ref,
+	shallowRef,
+	computed,
+	watch,
+	markRaw,
+	onMounted,
+	nextTick,
+	onActivated,
+	onBeforeUnmount,
+	watchSyncEffect,
 } from "vue";
 
 defineOptions({
-  name: 'ECharts',
-})
+	name: "ECharts",
+});
 const props = withDefaults(defineProps<Props>(), {
-  renderer: 'canvas',
-  resize: true,
-  theme: 'light'
-})
+	renderer: "canvas",
+	resize: true,
+	theme: "light",
+});
 const emit = defineEmits<{
-  click: [ECElementEvent]
-}>()
+	click: [ECElementEvent];
+}>();
 
 interface Props {
-  option: ECOption
-  renderer?: 'canvas' | 'svg'
-  resize?: boolean
-  width?: number | string
-  height?: number | string
-  loading?: boolean
-  theme?: 'dark' | 'light'
+	option: ECOption;
+	renderer?: "canvas" | "svg";
+	resize?: boolean;
+	width?: number | string;
+	height?: number | string;
+	loading?: boolean;
+	theme?: "dark" | "light" | string;
 }
 
-const chartRef = ref<HTMLDivElement | HTMLCanvasElement>()
-const chartInstance = shallowRef<EChartsType>()
+const chartRef = ref<HTMLDivElement | HTMLCanvasElement>();
+const chartInstance = shallowRef<EChartsType>();
 
-const chartTheme = computed(() => props.theme === 'dark' ? 'dark' : null)
+const chartTheme = computed(() => (props.theme === "dark" ? "dark" : null));
 const options = computed(() => {
-  return {
-    ...props.option,
-    backgroundColor: '',
-  }
-})
+	return {
+		...props.option,
+		backgroundColor: "",
+	};
+});
 
 function draw() {
-  if (chartInstance.value) {
-    chartInstance.value.setOption(options.value, { notMerge: true })
-  }
+	if (chartInstance.value) {
+		chartInstance.value.setOption(options.value, { notMerge: true });
+	}
 }
 
-watch(props.option, () => {
-  draw()
-}, { deep: true })
+watch(
+	props.option,
+	() => {
+		draw();
+	},
+	{ deep: true },
+);
 
 function handleClick(event: ECElementEvent) {
-  emit('click', event)
+	emit("click", event);
 }
 
 function init() {
-  if (!chartRef.value)
-    return
-  if (chartInstance.value) {
-    chartInstance.value.dispose()
-  }
+	if (!chartRef.value) return;
+	if (chartInstance.value) {
+		chartInstance.value.dispose();
+	}
 
-  chartInstance.value = echarts.getInstanceByDom(chartRef.value)
+	chartInstance.value = echarts.getInstanceByDom(chartRef.value);
 
-  if (!chartInstance.value) {
-    chartInstance.value = markRaw(
-      echarts.init(chartRef.value, chartTheme.value, {
-        renderer: props.renderer,
-      }),
-    )
-    chartInstance.value.on('click', handleClick)
-    draw()
-  }
+	if (!chartInstance.value) {
+		chartInstance.value = markRaw(
+			echarts.init(chartRef.value, chartTheme.value, {
+				renderer: props.renderer,
+			}),
+		);
+		chartInstance.value.on("click", handleClick);
+		draw();
+	}
 }
 
 function resize() {
-  if (chartInstance.value && props.resize) {
-    chartInstance.value.resize({ animation: { duration: 300 } })
-  }
+	if (chartInstance.value && props.resize) {
+		chartInstance.value.resize({ animation: { duration: 300 } });
+	}
 }
 
-const debouncedResize = useDebounceFn(resize, 300, { maxWait: 800 })
+const debouncedResize = useDebounceFn(resize, 300, { maxWait: 800 });
 
-useResizeObserver(chartRef, debouncedResize)
+useResizeObserver(chartRef, debouncedResize);
 
 onMounted(() => {
-  nextTick(() => init())
-})
+	nextTick(() => init());
+});
 
 watch(
-  () => props.theme,
-  () => {
-    if (chartInstance.value) {
-      init()
-    }
-  },
-)
+	() => props.theme,
+	() => {
+		if (chartInstance.value) {
+			init();
+		}
+	},
+);
 
 onActivated(() => {
-  if (chartInstance.value) {
-    chartInstance.value.resize()
-  }
-})
+	if (chartInstance.value) {
+		chartInstance.value.resize();
+	}
+});
 
 onBeforeUnmount(() => {
-  chartInstance.value?.dispose()
-})
+	chartInstance.value?.dispose();
+});
 
 watchSyncEffect(() => {
-  if (chartInstance.value) {
-    props.loading ? chartInstance.value.showLoading() : chartInstance.value.hideLoading()
-  }
-})
+	if (chartInstance.value) {
+		props.loading
+			? chartInstance.value.showLoading()
+			: chartInstance.value.hideLoading();
+	}
+});
 
 defineExpose({
-  getInstance: () => chartInstance.value,
-  resize,
-  draw,
-})
+	getInstance: () => chartInstance.value,
+	resize,
+	draw,
+});
 </script>
 
 <template>
